e5c03bd678327eb144cf788b47aa90d812db85f3
Igor Pimentel <igor.pimentel.msi@hotmail.com>
2023-01-03 22:38:32 -0300

    🧑‍💻 chore: add path mapping

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'node:path'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+	resolve: {
+		alias: {
+			'@assets': path.resolve(__dirname, './src/assets'),
+			'@components': path.resolve(__dirname, './src/components'),
+			'@pages': path.resolve(__dirname, './src/pages'),
+			'@theme': path.resolve(__dirname, './src/theme')
+		}
+	}
 })
